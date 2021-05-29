9185f3c041452d27ecf95a25c9b12f961b282d41
nielhuaman <73756567+nielhuaman@users.noreply.github.com>
2021-05-28 23:06:22 -0500

    28052021-4
    28052021-4

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+
+    public function run()
+    {
+        \DB::table('users')->insert([[
+            'name' => 'admin',
+            'email'     => 'admin@gmail.com',
+            'password'  => bcrypt('admin'),
+            'updated_at'=> "2017-11-24 15:55:32",
+            'created_at'=> "2017-11-24 15:55:32",
+        ],[
+            'name' => 'nhuaman',
+            'email'     => 'nhuamancerva@gmail.com',
+            'password'  => bcrypt('12345678'),
+            'updated_at'=> "2017-11-24 15:55:32",
+            'created_at'=> "2017-11-24 15:55:32",
+        ]]);
+    }
+}
